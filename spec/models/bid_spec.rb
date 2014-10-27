457a541c70a6e3c5411169c4d1bdcfc365746524
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2014-10-27 15:23:28 +0000

    spec de invite

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Bid, :type => :model do
+  fixtures :bids, :users
+
   it { is_expected.to belong_to :parent }
   it { is_expected.to belong_to(:owner)
                         .with_foreign_key(:owner_id)
                         .class_name('User') }
   it { is_expected.to have_and_belong_to_many(:bidders) }
 
-  it '' do
-    expect { subject.invite!(1234) }.to raise_error "user deve ser um User, email ou hash de parametros para o invite"
+  context '#invite' do
+    it 'Exceptions on non user' do
+      expect { subject.invite!(1234) }.to raise_error "user deve ser um User, email ou hash de parametros para o invite"
+    end
+
+    it 'User must add to bidders collection' do
+      bid = bids(:bid1)
+      b1 = users(:bidder1)
+      b2 = users(:bidder2)
+      bid.invite!(b1)
+      bid.invite!(b2)
+      expect(bid.bidders).to eq([b1, b2])
+    end
   end
 end
