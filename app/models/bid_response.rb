6d7eeced367878b071685029942963bf5b4cda5e
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2014-11-04 23:10:47 +0000

    bidder association

--- app/models/bid_response.rb
+++ app/models/bid_response.rb
@@ -1,3 +1,4 @@
 class BidResponse < ActiveRecord::Base
   belongs_to :bid
+  belongs_to :bidder, class_name: 'User' #, foreign_key: 'bidder_id'
 end
--- spec/models/bid_response_spec.rb
+++ spec/models/bid_response_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe BidResponse, :type => :model do
   it { is_expected.to belong_to :bid }
+  it { is_expected.to belong_to :bidder }
 end
