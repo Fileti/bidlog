249a2cde3abe4f66fe08aa572a23204e589221e7
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2014-11-28 18:16:17 +0000

    Mudancas no layout, ajustes nos acentos
    O arquivo de tradução do devise estava !@##$#@ :Please
    
    fixes #18
    fixes #15

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :invitable
+         :recoverable, :rememberable, :trackable, :validatable
   
   validates :email, presence: true, uniqueness: true
 
