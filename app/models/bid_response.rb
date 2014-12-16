9e7e808369fa2f46c304d14d9af2bea414475c62
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2014-12-16 17:49:54 +0000

    Refactory: tudo para scope

--- app/models/bid_response.rb
+++ app/models/bid_response.rb
@@ -3,4 +3,15 @@ class BidResponse < ActiveRecord::Base
 
   belongs_to :bid
   belongs_to :bidder, class_name: 'User' #, foreign_key: 'bidder_id'
+
+  scope :bids_responded, ->(user) do
+    joins(:bid)
+      .where(bidder: user)
+  end
+
+  scope :bids_responded_ids, ->(user) do
+    select(:bid_id)
+      .where(bidder: user)
+      .uniq
+  end
 end
